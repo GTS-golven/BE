c0367fa9c2010d737ee19acdf431ce19018bd1c5
LiranKadin <92515857+LiranKadin@users.noreply.github.com>
2022-03-16 12:07:46 +0100

    dingen geupdate

--- GTS-BE/Src/BE/Videos/urls.py
+++ GTS-BE/Src/BE/Videos/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
     path('videos/', views.LoginView.as_view()),
