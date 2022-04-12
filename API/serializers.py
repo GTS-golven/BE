cf35cd6fa88397cfa826623780baeac4d05f2a6e
LiranKadin <92515857+LiranKadin@users.noreply.github.com>
2022-04-12 10:26:24 +0200

    updated views.py

--- API/serializers.py
+++ API/serializers.py
@@ -1,20 +1,31 @@
+from typing_extensions import Self
 from rest_framework import serializers
 from API.models import User, video, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+User = get_user_model()
 
-class APISerializer(serializers.Serializer):
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-    # video serializers
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    Id = serializers.IntegerField(read_only=True)
-    GolfClub = serializers.CharField()
-    GolfCourse = serializers.CharField()
-    description = serializers.CharField()
-    video = serializers.FileField()
-    date = serializers.DateTimeField()
-    User = serializers.IntegerField()
+
+# class APISerializer(serializers.Serializer):
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+#     # video serializers
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     Id = serializers.IntegerField(read_only=True)
+#     GolfClub = serializers.CharField()
+#     GolfCourse = serializers.CharField()
+#     description = serializers.CharField()
+#     video = serializers.FileField()
+#     date = serializers.DateTimeField()
+#     User = serializers.IntegerField(User)
+#     # user = User(**validated_data) 
+
+class APISerializer(serializers.ModelSerializer):
+    class Meta:
+        model = video
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
 
 
     def create(self, validated_data):
@@ -23,7 +34,7 @@ class APISerializer(serializers.Serializer):
         """
         return video.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+    def update(self, instance, validated_data, user):
         """
         Update and return an existing `video` instance, given the validated data.
         """
@@ -32,6 +43,13 @@ class APISerializer(serializers.Serializer):
         instance.linenos = validated_data.get('linenos', instance.linenos)
         instance.language = validated_data.get('language', instance.language)
         instance.style = validated_data.get('style', instance.style)
+        instance.GolfClub = validated_data.get('GolfClub', instance.GolfClub)
+        instance.GolfCourse = validated_data.get('GolfCourse', instance.GolfCourse)
+        instance.Id = validated_data.get('Id', instance.Id)
+        instance.description = validated_data.get('description', instance.description)
+        instance.date = validated_data.get('date', instance.date)
+        instance.video = validated_data.get('video', instance.video)
+        user = User(**validated_data, self)
         instance.save()
         return instance
 
--- API/views.py
+++ API/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from API.models import video
+from API.serializers import APISerializer
 
 # Create your views here.
+
+
+@csrf_exempt
+def video_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = video.objects.all()
+        serializer = APISerializer(video, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = APISerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
