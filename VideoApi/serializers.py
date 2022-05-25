b5c3273c910d5f963356ec0038a29491b615e7b0
LiranKadin <92515857+LiranKadin@users.noreply.github.com>
2022-05-25 10:36:31 +0200

    base64

--- VideoApi/serializers.py
+++ VideoApi/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Video
 import os
+import base64
 
 class VideoSerializer(serializers.ModelSerializer):
-    video = serializers.FileField(max_length=None, use_url=True)
+    video = serializers.CharField()
 
     class Meta:
         model = Video
