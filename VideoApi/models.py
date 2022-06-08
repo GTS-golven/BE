b0daa1a6142169cf0a97db17ecb405974123cfc0
LiranKadin <92515857+LiranKadin@users.noreply.github.com>
2022-06-08 12:03:09 +0200

    added title etc

--- VideoApi/models.py
+++ VideoApi/models.py
@@ -5,5 +5,12 @@ from django.core.files.base import ContentFile
 # Create your models here.
 class Videos(models.Model):
     video = models.FileField(upload_to='videos')
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    golfclub = models.CharField(max_length=10, blank=True)
+    golfbaan = models.CharField(max_length=50, blank=True)
+    datum = models.DateTimeField()
+    rpm = models.IntegerField()
+    height = models.IntegerField()
 
 
